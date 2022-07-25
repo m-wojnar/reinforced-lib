7c3339808a22614834562d9b88eadf382a038a36
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2022-07-25 16:54:41 +0200

    Split Particle Filter into base algorithm and specialized Wi-Fi agent

---
+++ reinforced_lib/agents/core/particle_filter.py
@@ -0,0 +1,360 @@
+from functools import partial
+from typing import Any, Callable, Tuple
+
+import distrax
+import jax
+import jax.numpy as jnp
+from chex import dataclass, Array, Numeric, PRNGKey, Scalar, Shape
+
+from reinforced_lib.agents.base_agent import AgentState
+
+
+@dataclass
+class ParticleFilterState(AgentState):
+    """
+    Container for the state of the Particle Filter.
+
+    Attributes
+    ----------
+    positions : array_like
+        Positions of filter particles.
+    weights : array_like
+        Weights of filter particles.
+    last_measurement : float
+        Time of the last update.
+    """
+
+    positions: Array
+    logit_weights: Array
+    last_measurement: Scalar
+
+
+def simple_resample(operands: Tuple[ParticleFilterState, PRNGKey]) -> ParticleFilterState:
+    """
+    Samples new particles positions from categorical distribution based on weights.
+    Sets all weights to be equal.
+
+    Parameters
+    ----------
+    operands : tuple[ParticleFilterState, PRNGKey]
+        Tuple containing filter state and a PRNG key.
+
+    Returns
+    -------
+    state : ParticleFilterState
+        Updated filter state.
+    """
+
+    state, key = operands
+    logit_weights = state.logit_weights - jnp.max(state.logit_weights)
+    positions_idx = distrax.Categorical(logit_weights).sample(seed=key, sample_shape=state.positions.shape)
+
+    return ParticleFilterState(
+        positions=state.positions[positions_idx],
+        logit_weights=jnp.zeros_like(logit_weights),
+        last_measurement=state.last_measurement
+    )
+
+
+def effective_sample_size(state: ParticleFilterState, threshold: Scalar = 0.5) -> bool:
+    """
+    Calculates effective sample size [1]_. If ESS is smaller than number of samples * threshold,
+    than resampling is necessary.
+
+    Parameters
+    ----------
+    state : ParticleFilterState
+        Current state of the filter.
+    threshold : float, default=0.5
+        Threshold value used to decide if resampling is necessary.
+
+    Returns
+    -------
+    perform_resampling : bool
+        Information whether resampling should be performed.
+
+    References
+    ----------
+    .. [1] https://en.wikipedia.org/wiki/Effective_sample_size#Weighted_samples
+    """
+
+    logit_weights = state.logit_weights - jnp.max(state.logit_weights)
+    weights = jnp.exp(logit_weights)
+    weights = weights / jnp.sum(weights)
+    sample_size = 1 / jnp.sum(weights ** 2)
+
+    return sample_size < state.positions.size * threshold
+
+
+def simple_transition(state: ParticleFilterState, key: PRNGKey, scale: Scalar, *args) -> ParticleFilterState:
+    """
+    Performs simple movement of particles positions based on normal distribution with
+    mean = 0 and standard deviation = scale.
+
+    Parameters
+    ----------
+    state : ParticleFilterState
+        Current state of the filter.
+    key : PRNGKey
+        A PRNG key used as the random key.
+    scale : float
+        Scale of the random movement of particles.
+
+    Returns
+    -------
+    state : ParticleFilterState
+        Updated filter state.
+    """
+
+    positions_update = distrax.Normal(0, scale).sample(sample_shape=state.positions.shape, seed=key)
+
+    return ParticleFilterState(
+        positions=state.positions + positions_update,
+        logit_weights=state.logit_weights,
+        last_measurement=state.last_measurement
+    )
+
+
+def linear_transition(state: ParticleFilterState, key: PRNGKey, scale: Scalar, time: Scalar) -> ParticleFilterState:
+    """
+    Performs movement of particles positions based on normal distribution with
+    mean = 0 and standard deviation = scale * (last_measurement - time).
+
+    Parameters
+    ----------
+    state : ParticleFilterState
+        Current state of the filter.
+    key : PRNGKey
+        A PRNG key used as the random key.
+    scale : float
+        Scale of the random movement of particles.
+    time : float
+        Current time.
+
+    Returns
+    -------
+    state : ParticleFilterState
+        Updated filter state.
+    """
+
+    return simple_transition(state, key, scale * (state.last_measurement - time))
+
+
+def affine_transition(state: ParticleFilterState, key: PRNGKey, scale: Array, time: Scalar) -> ParticleFilterState:
+    """
+    Performs movement of particles positions based on normal distribution with
+    mean = 0 and standard deviation = scale_a * (last_measurement - time) + scale_b.
+
+    Parameters
+    ----------
+    state : ParticleFilterState
+        Current state of the filter.
+    key : PRNGKey
+        A PRNG key used as the random key.
+    scale : array_like
+        Scale of the random movement of particles.
+    time : float
+        Current time.
+
+    Returns
+    -------
+    state : ParticleFilterState
+        Updated filter state.
+    """
+
+    return simple_transition(state, key, scale[0] * (state.last_measurement - time) + scale[1])
+
+
+class ParticleFilter:
+    """
+    Particle Filter (Sequential Monte Carlo) algorithm estimating
+    internal environment state given noisy or partial observations.
+
+    Parameters
+    ----------
+    initial_distribution : distrax.Distribution
+        Distribution from which the initial state is sampled.
+    positions_shape : array_like
+        Shape of the particles positions array.
+    weights_shape : array_like
+        Shape of the particles weights array.
+    scale : float or array_like
+        Scale of the random movement of particles.
+    observation_fn : callable
+        Function that updates particles based on the observation of the environment, takes two positional arguments:
+            - ``state``: state of the filter (`ParticleFilterState`).
+            - ``observation``: observation of the environment (`any`).
+        Returns updated state of the filter (`ParticleFilterState`).
+    resample_fn : callable, default=particle_filter.simple_resample
+        Function that performs resampling of particles, takes one positional argument:
+            - ``operands``: tuple containing filter state and a PRNG key (`tuple[ParticleFilterState, PRNGKey]`).
+        Returns updated state of the filter (`ParticleFilterState`).
+    resample_criterion_fn : callable, default=particle_filter.effective_sample_size
+        Function that checks if resampling is necessary, takes one positional argument:
+            - ``state``: state of the filter (`ParticleFilterState`).
+        Returns information whether resampling should be performed (`bool`).
+    transition_fn : callable, default=particle_filter.simple_transition
+        Function that updates particles positions, takes four positional arguments:
+            - ``state``: state of the filter (`ParticleFilterState`).
+            - ``key``: a PRNG key used as the random key (`PRNGKey`).
+            - ``scale``: scale of the random movement of particles (`float or array_like`).
+            - ``time``: current time (`float`).
+        Returns updated state of the filter (`ParticleFilterState`).
+    """
+
+    def __init__(
+            self,
+            initial_distribution: distrax.Distribution,
+            positions_shape: Shape,
+            weights_shape: Shape,
+            scale: Numeric,
+            observation_fn: Callable[[ParticleFilterState, Any], ParticleFilterState],
+            resample_fn: Callable[[Tuple[ParticleFilterState, PRNGKey]], ParticleFilterState] = simple_resample,
+            resample_criterion_fn: Callable[[ParticleFilterState], bool] = effective_sample_size,
+            transition_fn: Callable[[ParticleFilterState, PRNGKey, Numeric, Scalar], ParticleFilterState] = simple_transition
+    ) -> None:
+        self.init = jax.jit(
+            partial(
+                self.init,
+                initial_distribution=initial_distribution,
+                positions_shape=positions_shape,
+                weights_shape=weights_shape
+            )
+        )
+        self.update = jax.jit(
+            partial(
+                self.update,
+                observation_fn=observation_fn,
+                resample_fn=resample_fn,
+                resample_criterion_fn=resample_criterion_fn,
+                transition_fn=transition_fn,
+                scale=scale
+            )
+        )
+        self.sample = jax.jit(self.sample)
+
+    @staticmethod
+    def init(
+            key: PRNGKey,
+            initial_distribution: distrax.Distribution,
+            positions_shape: Shape,
+            weights_shape: Shape
+    ) -> ParticleFilterState:
+        """
+        Creates and initializes instance of the Particle Filter.
+
+        Parameters
+        ----------
+        key : PRNGKey
+            A PRNG key used as the random key.
+        initial_distribution : distrax.Distribution
+            Distribution from which the initial state is sampled.
+        positions_shape : array_like
+            Shape of the particles positions array.
+        weights_shape : array_like
+            Shape of the particles weights array.
+
+        Returns
+        -------
+        state : ParticleFilterState
+            Initial state of the Particle Filter.
+        """
+
+        return ParticleFilterState(
+            positions=initial_distribution.sample(sample_shape=positions_shape, seed=key),
+            logit_weights=jnp.zeros(weights_shape),
+            last_measurement=0.0
+        )
+
+    @staticmethod
+    def update(
+            state: ParticleFilterState,
+            key: PRNGKey,
+            observation_fn: Callable[[ParticleFilterState, Any], ParticleFilterState],
+            observation: Any,
+            resample_fn: Callable[[Tuple[ParticleFilterState, PRNGKey]], ParticleFilterState],
+            resample_criterion_fn: Callable[[ParticleFilterState], bool],
+            transition_fn: Callable[[ParticleFilterState, PRNGKey, Numeric, Scalar], ParticleFilterState],
+            time: Scalar,
+            measurement_time: Scalar,
+            scale: Numeric
+    ) -> ParticleFilterState:
+        """
+        Updates the state of the filter based on the observation of the environment,
+        performs resampling (if necessary) and transition of particles.
+
+        Parameters
+        ----------
+        state : ParticleFilterState
+            Current state of the filter.
+        key : PRNGKey
+            A PRNG key used as the random key.
+        observation_fn : callable
+            Function that updates particles based on the observation of the environment, takes two positional arguments:
+                - ``state``: state of the filter (`ParticleFilterState`).
+                - ``observation``: observation of the environment (`any`).
+            Returns updated state of the filter (`ParticleFilterState`).
+        observation : any
+            Observation of the environment.
+        resample_fn : callable
+            Function that performs resampling of particles, takes one positional argument:
+                - ``operands``: tuple containing filter state and a PRNG key (`tuple[ParticleFilterState, PRNGKey]`).
+            Returns updated state of the filter (`ParticleFilterState`).
+        resample_criterion_fn : callable
+            Function that checks if resampling is necessary, takes one positional argument:
+                - ``state``: state of the filter (`ParticleFilterState`).
+            Returns information whether resampling should be performed (`bool`).
+        transition_fn : callable
+            Function that updates particles positions, takes four positional arguments:
+                - ``state``: state of the filter (`ParticleFilterState`).
+                - ``key``: a PRNG key used as the random key (`PRNGKey`).
+                - ``scale``: scale of the random movement of particles (`float or array_like`).
+                - ``time``: current time (`float`).
+            Returns updated state of the filter (`ParticleFilterState`).
+        time : float
+            Current time.
+        measurement_time : float
+            Last measurement time.
+        scale : float or array_like
+            Scale of the random movement of particles.
+
+        Returns
+        -------
+        state : ParticleFilterState
+            Updated filter state.
+        """
+
+        resample_key, transition_key = jax.random.split(key)
+
+        state = observation_fn(state, observation)
+        state = jax.lax.cond(resample_criterion_fn(state), resample_fn, lambda op: op[0], (state, resample_key))
+        state = transition_fn(state, transition_key, scale, time)
+
+        return ParticleFilterState(
+            positions=state.positions,
+            logit_weights=state.logit_weights,
+            last_measurement=measurement_time
+        )
+
+    @staticmethod
+    def sample(
+            state: ParticleFilterState,
+            key: PRNGKey
+    ) -> Tuple[ParticleFilterState, Numeric]:
+        """
+        Samples estimated environment state based on the current filter state.
+
+        Parameters
+        ----------
+        state : ParticleFilterState
+            Current state of the filter.
+        key : PRNGKey
+            A PRNG key used as the random key.
+
+        Returns
+        -------
+        tuple[ParticleFilterState, float or array_like]
+            Tuple containing filter state and estimated state.
+        """
+
+        return state, state.positions[distrax.Categorical(state.logit_weights).sample(seed=key)]
--- reinforced_lib/agents/wifi/particle_filter.py
+++ reinforced_lib/agents/wifi/particle_filter.py
@@ -1,39 +1,21 @@
 from functools import partial
 from typing import Tuple
 
-import chex
 import distrax
 import gym.spaces
 import jax
 import jax.numpy as jnp
+from chex import Array, PRNGKey, Scalar
 from jax.scipy.special import erf
 
-from reinforced_lib.agents.base_agent import BaseAgent, AgentState
-
-
-@chex.dataclass
-class ParticleFilterState(AgentState):
-    """
-    Container for the state of the Particle Filter agent.
-
-    Attributes
-    ----------
-    positions : array_like
-        Positions of filter particles.
-    weights : array_like
-        Weights of filter particles.
-    last_sample : float
-        Time of the last sampling.
-    """
-
-    positions: chex.Array
-    logit_weights: chex.Array
-    last_sample: chex.Scalar
+from reinforced_lib.agents.base_agent import BaseAgent
+from reinforced_lib.agents.core.particle_filter import ParticleFilter as ParticleFilterBase
+from reinforced_lib.agents.core.particle_filter import ParticleFilterState
 
 
 class ParticleFilter(BaseAgent):
     """
-    Particle Filter agent designed for IEEE 802.11 environments. Implementation based on [2]_.
+    Particle Filter agent designed for IEEE 802.11 environments. Implementation based on [1]_.
 
     Parameters
     ----------
@@ -52,31 +34,31 @@ class ParticleFilter(BaseAgent):
 
     References
     ----------
-    .. [2] Krotov, Alexander & Kiryanov, Anton & Khorov, Evgeny. (2020). Rate Control With Spatial Reuse
+    .. [1] Krotov, Alexander & Kiryanov, Anton & Khorov, Evgeny. (2020). Rate Control With Spatial Reuse
        for Wi-Fi 6 Dense Deployments. IEEE Access. 8. 168898-168909. 10.1109/ACCESS.2020.3023552.
     """
 
     def __init__(
             self,
             n_mcs: jnp.int32,
-            min_snr: chex.Scalar,
-            max_snr: chex.Scalar,
-            initial_power: chex.Scalar,
+            min_snr: Scalar,
+            max_snr: Scalar,
+            initial_power: Scalar,
             particles_num: jnp.int32 = 1000,
-            scale: chex.Scalar = 10.0
+            scale: Scalar = 10.0
     ) -> None:
         self.n_mcs = n_mcs
 
-        self.init = jax.jit(
-            partial(
-                self.init,
-                min_position=min_snr - initial_power,
-                max_position=max_snr - initial_power,
-                particles_num=particles_num
-            )
+        self.pf = ParticleFilterBase(
+            initial_distribution=distrax.Uniform(min_snr - initial_power, max_snr - initial_power),
+            positions_shape=(particles_num,),
+            weights_shape=(particles_num,),
+            scale=scale,
+            observation_fn=jax.jit(self._observation_fn)
         )
-        self.update = jax.jit(partial(self.update, scale=scale))
-        self.sample = jax.jit(self.sample)
+
+        self.update = partial(self.update, scale=scale)
+        self.sample = jax.jit(partial(self.sample, pf=self.pf))
 
     @staticmethod
     def parameters_space() -> gym.spaces.Dict:
@@ -98,7 +80,7 @@ class ParticleFilter(BaseAgent):
             'time': gym.spaces.Box(0.0, jnp.inf, (1,)),
             'power': gym.spaces.Box(-jnp.inf, jnp.inf, (1,)),
             'cw': gym.spaces.Discrete(32767),
-            'min_snr': gym.spaces.Box(-jnp.inf, jnp.inf, (self.n_mcs,))
+            'min_snrs': gym.spaces.Box(-jnp.inf, jnp.inf, (self.n_mcs,))
         })
 
     @property
@@ -107,30 +89,21 @@ class ParticleFilter(BaseAgent):
             'time': gym.spaces.Box(0.0, jnp.inf, (1,)),
             'power': gym.spaces.Box(-jnp.inf, jnp.inf, (1,)),
             'rates': gym.spaces.Box(0.0, jnp.inf, (self.n_mcs,)),
-            'min_snr': gym.spaces.Box(-jnp.inf, jnp.inf, (self.n_mcs,))
+            'min_snrs': gym.spaces.Box(-jnp.inf, jnp.inf, (self.n_mcs,))
         })
 
     @property
     def action_space(self) -> gym.spaces.Discrete:
         return gym.spaces.Discrete(self.n_mcs)
 
-    @staticmethod
-    def init(
-            min_position: chex.Scalar,
-            max_position: chex.Scalar,
-            particles_num: jnp.int32
-    ) -> ParticleFilterState:
+    def init(self, key: PRNGKey) -> ParticleFilterState:
         """
         Creates and initializes instance of the Particle Filter agent.
 
         Parameters
         ----------
-        min_position : float
-            Minimum position of a particle.
-        max_position : float
-            Maximum position of a particle.
-        particles_num : int
-            Number of created particles.
+        key : PRNGKey
+            A PRNG key used as the random key.
 
         Returns
         -------
@@ -138,24 +111,20 @@ class ParticleFilter(BaseAgent):
             Initial state of the Particle Filter agent.
         """
 
-        return ParticleFilterState(
-            positions=jnp.linspace(min_position, max_position, particles_num),
-            logit_weights=jnp.zeros(particles_num),
-            last_sample=0.0
-        )
+        return self.pf.init(key)
 
-    @staticmethod
     def update(
+            self,
             state: ParticleFilterState,
-            key: chex.PRNGKey,
+            key: PRNGKey,
             action: jnp.int32,
             n_successful: jnp.int32,
             n_failed: jnp.int32,
-            time: chex.Scalar,
-            power: chex.Scalar,
+            time: Scalar,
+            power: Scalar,
             cw: jnp.int32,
-            min_snr: chex.Array,
-            scale: chex.Scalar
+            scale: Scalar,
+            min_snrs: Array
     ) -> ParticleFilterState:
         """
         Updates the state of the agent after performing some action and receiving a reward.
@@ -164,7 +133,7 @@ class ParticleFilter(BaseAgent):
         ----------
         state : ParticleFilterState
             Current state of agent.
-        key : chex.PRNGKey
+        key : PRNGKey
             A PRNG key used as the random key.
         action : int
             Previously selected action.
@@ -178,10 +147,10 @@ class ParticleFilter(BaseAgent):
             Power used during the transmission [dBm].
         cw : int
             Contention Window used during the transmission.
-        min_snr : array_like
-            Minimal SNR value that is required for a successful transmission for each MCS [dBm].
         scale : float
             Velocity of the random movement of particles.
+        min_snrs : array_like
+            Minimal SNR value that is required for a successful transmission for each MCS [dBm].
 
         Returns
         -------
@@ -189,61 +158,24 @@ class ParticleFilter(BaseAgent):
             Updated agent state.
         """
 
-        def on_success(operands: Tuple) -> ParticleFilterState:
-            state, n, cw, p_s = operands
-            return ParticleFilterState(
-                positions=state.positions,
-                logit_weights=state.logit_weights + n * jnp.log(p_s * (1 - 1 / cw)),
-                last_sample=state.last_sample
-            )
-
-        def on_failure(operands: Tuple) -> ParticleFilterState:
-            state, n, cw, p_s = operands
-            return ParticleFilterState(
-                positions=state.positions,
-                logit_weights=state.logit_weights + n * jnp.log(1 - p_s * (1 - 1 / cw)),
-                last_sample=state.last_sample
-            )
-
-        def resample(operands: Tuple) -> ParticleFilterState:
-            state, logit_weights, key = operands
-            positions_idx = distrax.Categorical(logit_weights).sample(seed=key, sample_shape=state.positions.size)
-            return ParticleFilterState(
-                positions=state.positions[positions_idx],
-                logit_weights=jnp.zeros_like(logit_weights),
-                last_sample=state.last_sample
-            )
-
-        particles_num = state.positions.size
-        success_prob_func = jax.vmap(ParticleFilter._success_probability, in_axes=[None, 0])
-        p_s = success_prob_func(min_snr[action], state.positions + power)
-
-        state = jax.lax.cond(n_successful > 0, on_success, lambda op: op[0], (state, n_successful, cw, p_s))
-        state = jax.lax.cond(n_failed > 0, on_failure, lambda op: op[0], (state, n_failed, cw, p_s))
-
-        logit_weights = state.logit_weights - jnp.max(state.logit_weights)
-        weights = jnp.exp(logit_weights)
-        weights = weights / jnp.sum(weights)
-        sample_size = 1 / jnp.sum(weights ** 2)
-        state = jax.lax.cond(sample_size < particles_num / 2, resample, lambda op: op[0], (state, logit_weights, key))
-
-        movement_scale = scale * (time - state.last_sample)
-        positions = state.positions + distrax.Normal(0, movement_scale).sample(seed=key, sample_shape=particles_num)
-
-        return ParticleFilterState(
-            positions=positions,
-            logit_weights=state.logit_weights,
-            last_sample=state.last_sample
+        return self.pf.update(
+            state=state,
+            key=key,
+            observation=(action, n_successful, n_failed, power, cw, min_snrs),
+            time=time,
+            measurement_time=time,
+            scale=scale
         )
 
     @staticmethod
     def sample(
             state: ParticleFilterState,
-            key: chex.PRNGKey,
-            time: chex.Scalar,
-            power: chex.Scalar,
-            rates: chex.Array,
-            min_snr: chex.Array
+            key: PRNGKey,
+            time: Scalar,
+            power: Scalar,
+            rates: Array,
+            min_snrs: Array,
+            pf: ParticleFilterBase,
     ) -> Tuple[ParticleFilterState, jnp.int32]:
         """
         Selects next action based on current agent state.
@@ -252,7 +184,7 @@ class ParticleFilter(BaseAgent):
         ----------
         state : ParticleFilterState
             Current state of the agent.
-        key : chex.PRNGKey
+        key : PRNGKey
             A PRNG key used as the random key.
         time : float
             Current time [s].
@@ -260,8 +192,10 @@ class ParticleFilter(BaseAgent):
             Power used during the transmission [dBm].
         rates : array_like
             Transmission data rates corresponding to each MCS [Mb/s].
-        min_snr : array_like
+        min_snrs : array_like
             Minimal SNR value that is required for a successful transmission for each MCS [dBm].
+        pf : ParticleFilterBase
+            Instance of the base ParticleFilter class.
 
         Returns
         -------
@@ -269,24 +203,69 @@ class ParticleFilter(BaseAgent):
             Tuple containing updated agent state and selected action.
         """
 
-        success_prob_func = jax.vmap(ParticleFilter._success_probability, in_axes=[0, None])
-        snr_sample = state.positions[distrax.Categorical(state.logit_weights).sample(seed=key)] + power
-        p_s = success_prob_func(min_snr, snr_sample)
+        success_probability_fn = jax.vmap(ParticleFilter._success_probability, in_axes=[0, None])
+
+        _, snr_sample = pf.sample(state, key)
+        p_s = success_probability_fn(min_snrs, snr_sample + power)
 
         action = jnp.argmax(p_s * rates)
         state = ParticleFilterState(
             positions=state.positions,
             logit_weights=state.logit_weights,
-            last_sample=time
+            last_measurement=time
         )
 
         return state, action
 
     @staticmethod
-    def _success_probability(
-            min_snr: chex.Scalar,
-            observed_snr: chex.Scalar
-    ) -> chex.Scalar:
+    def _observation_fn(
+            state: ParticleFilterState,
+            observation: Tuple[jnp.int32, jnp.int32, jnp.int32, Scalar, jnp.int32, Array]
+    ) -> ParticleFilterState:
+        """
+        Updates particles weights based on the observation of the environment.
+
+        Parameters
+        ----------
+        state : ParticleFilterState
+            Current state of the agent.
+        observation : tuple
+            Tuple containing action, n_successful, n_failed, power, cw, and min_snrs.
+
+        Returns
+        -------
+        state : ParticleFilterState
+            Updated state of the agent.
+        """
+
+        def on_success(operands: Tuple) -> ParticleFilterState:
+            state, n, cw, p_s = operands
+            return ParticleFilterState(
+                positions=state.positions,
+                logit_weights=state.logit_weights + n * jnp.log(p_s * (1 - 1 / cw)),
+                last_measurement=state.last_measurement
+            )
+
+        def on_failure(operands: Tuple) -> ParticleFilterState:
+            state, n, cw, p_s = operands
+            return ParticleFilterState(
+                positions=state.positions,
+                logit_weights=state.logit_weights + n * jnp.log(1 - p_s * (1 - 1 / cw)),
+                last_measurement=state.last_measurement
+            )
+
+        success_probability_fn = jax.vmap(ParticleFilter._success_probability, in_axes=[None, 0])
+
+        action, n_successful, n_failed, power, cw, min_snrs = observation
+        p_s = success_probability_fn(min_snrs[action], state.positions + power)
+
+        state = jax.lax.cond(n_successful > 0, on_success, lambda op: op[0], (state, n_successful, cw, p_s))
+        state = jax.lax.cond(n_failed > 0, on_failure, lambda op: op[0], (state, n_failed, cw, p_s))
+
+        return state
+
+    @staticmethod
+    def _success_probability(min_snr: Scalar, observed_snr: Scalar) -> Scalar:
         """
         Calculates approximated probability of a successful transmission for a given minimal and observed SNR.
 
