89d33c8c17f4075d2b9b8436ae8083215fabb26c
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2022-07-27 12:07:27 +0200

    Update setup.py requirements

--- setup.py
+++ setup.py
@@ -13,24 +13,16 @@ setup(
         'dm-haiku>=0.0.7',
         'gym>=0.25.0',
         'jax>=0.3.15',
-        'jaxlib>=0.3.15',
-        'numpy>=1.23.1',
+        'lz4>=4.0.2',
         'optax>=0.1.3',
-        'psutil>=5.7.2'
         'scipy>=1.8.1',
-        'tensorflow-macos>=2.9.2',
-        'tensorflow-metal>=0.5.0',
-        'tensorboard>=2.9.1',
     ],
     extras_require={
         'dev': [
-            'jupyter>=1.0.0',
-            'lz4>=4.0.2',
             'matplotlib>=3.5.2',
-            # 'ns3-ai==1.0.1',
-            'pandas>=1.4.3',
+            # 'ns3-ai==1.0.0',
             'seaborn>=0.11.2',
-            'sphinx>=5.1.0',
+            'sphinx>=5.1.1',
             'sphinx-rtd-theme>=1.0.0',
         ]
     }
