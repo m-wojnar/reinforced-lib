e8473f3907a41a3aa79bbe2727f10f92fd0015b6
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2022-07-28 13:46:12 +0200

    Add requirements files

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+from typing import List
+
 from setuptools import setup, find_packages
+
+
+def read_requirements(filename: str) -> List[str]:
+    with open(filename) as file:
+        return file.read().splitlines()
+
 
 setup(
     name='reinforced-lib',
@@ -7,23 +15,9 @@ setup(
         'reinforced_lib', 
         'reinforced_lib.*'
     ]),
-    install_requires=[
-        'chex>=0.1.3',
-        'distrax>=0.1.2',
-        'dm-haiku>=0.0.7',
-        'gym>=0.25.0',
-        'jax>=0.3.15',
-        'lz4>=4.0.2',
-        'optax>=0.1.3',
-        'scipy>=1.8.1',
-    ],
+    install_requires=read_requirements('requirements/requirements.txt'),
+    tests_require=read_requirements('requirements/requirements-dev.txt'),
     extras_require={
-        'dev': [
-            'matplotlib>=3.5.2',
-            # 'ns3-ai==1.0.0',
-            'seaborn>=0.11.2',
-            'sphinx>=5.1.1',
-            'sphinx-rtd-theme>=1.0.0',
-        ]
+        'dev': read_requirements('requirements/requirements-dev.txt')
     }
 )
