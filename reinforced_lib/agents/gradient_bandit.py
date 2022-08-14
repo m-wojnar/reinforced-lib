4b3eb30ded30a87543f7dda019f5369037dcf61a
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2022-08-14 14:39:03 +0200

    Add GradientBandit agent

--- reinforced_lib/agents/gradient_bandit.py
+++ reinforced_lib/agents/gradient_bandit.py
@@ -1,6 +1,7 @@
 from functools import partial
 from typing import Tuple
 
+import distrax
 import gym.spaces
 import jax
 import jax.numpy as jnp
@@ -10,56 +11,64 @@ from reinforced_lib.agents import BaseAgent, AgentState
 
 
 @dataclass
-class UCBState(AgentState):
+class GradientBanditState(AgentState):
     """
-    Container for the state of the UCB agent.
+    Container for the state of the gradient bandit agent.
 
     Attributes
     ----------
-    Q : array_like
-        Quality values for each arm
-    N : array_like
-        Number of tries for each arm
+    H : array_like
+        Preference for each arm.
+    R : float
+        Average of rewards for each arm.
+    n : int
+        Number of the step.
     """
 
-    Q: Array
-    N: Array
+    H: Array
+    R: Scalar
+    n: jnp.int64
 
 
-class UCB(BaseAgent):
+class GradientBandit(BaseAgent):
     """
-    UCB agent with optional exponential recency-weighted average update.
+    Gradient bandit agent with baseline and optional exponential recency-weighted average update.
+    Implementation inspired by [4]_.
 
     Parameters
     ----------
     n_arms : int
         Number of bandit arms.
-    c : float
-        Degree of exploration.
+    lr : float
+        Step size.
     alpha : float, default=0.0
         If non-zero than exponential recency-weighted average is used to update Q values. ``alpha`` must be in [0, 1).
+
+    References
+    ----------
+    .. [4]  Sutton, R. S., Barto, A. G. (2018). Reinforcement Learning: An Introduction. The MIT Press. 37-40.
     """
 
     def __init__(
             self,
             n_arms: jnp.int32,
-            c: Scalar,
+            lr: Scalar,
             alpha: Scalar = 0.0
     ) -> None:
-        assert c >= 0
+        assert lr > 0
         assert 0 <= alpha <= 1
 
         self.n_arms = n_arms
 
         self.init = jax.jit(partial(self.init, n_arms=n_arms))
-        self.update = jax.jit(partial(self.update, alpha=alpha))
-        self.sample = jax.jit(partial(self.sample, c=c))
+        self.update = jax.jit(partial(self.update, lr=lr, alpha=alpha))
+        self.sample = jax.jit(self.sample)
 
     @staticmethod
     def parameters_space() -> gym.spaces.Dict:
         return gym.spaces.Dict({
             'n_arms': gym.spaces.Box(1, jnp.inf, (1,), jnp.int32),
-            'c': gym.spaces.Box(0.0, jnp.inf, (1,), jnp.float32),
+            'lr': gym.spaces.Box(0.0, jnp.inf, (1,), jnp.float32),
             'alpha': gym.spaces.Box(0.0, 1.0, (1,), jnp.float32)
         })
 
@@ -72,9 +81,7 @@ class UCB(BaseAgent):
 
     @property
     def sample_observation_space(self) -> gym.spaces.Dict:
-        return gym.spaces.Dict({
-            'time': gym.spaces.Box(0.0, jnp.inf, (1,))
-        })
+        return gym.spaces.Dict({})
 
     @property
     def action_space(self) -> gym.spaces.Space:
@@ -84,9 +91,9 @@ class UCB(BaseAgent):
     def init(
             key: PRNGKey,
             n_arms: jnp.int32
-    ) -> UCBState:
+    ) -> GradientBanditState:
         """
-        Creates and initializes instance of the UCB agent.
+        Creates and initializes instance of the gradient bandit agent.
 
         Parameters
         ----------
@@ -97,29 +104,31 @@ class UCB(BaseAgent):
 
         Returns
         -------
-        state : UCBState
-            Initial state of the UCB agent.
+        state : GradientBanditState
+            Initial state of the gradient bandit agent.
         """
 
-        return UCBState(
-            Q=jnp.zeros(n_arms),
-            N=jnp.ones(n_arms, dtype=jnp.int32)
+        return GradientBanditState(
+            H=jnp.zeros(n_arms),
+            R=0.0,
+            n=1
         )
 
     @staticmethod
     def update(
-        state: UCBState,
+        state: GradientBanditState,
         key: PRNGKey,
         action: jnp.int32,
         reward: Scalar,
+        lr: Scalar,
         alpha: Scalar
-    ) -> UCBState:
+    ) -> GradientBanditState:
         """
         Updates the state of the agent after performing some action and receiving a reward.
 
         Parameters
         ----------
-        state : UCBState
+        state : GradientBanditState
             Current state of agent.
         key : PRNGKey
             A PRNG key used as the random key.
@@ -127,56 +136,63 @@ class UCB(BaseAgent):
             Previously selected action.
         reward : float
             Reward as a result of previous action.
+        lr : float
+            Step size.
         alpha : float
             Exponential recency-weighted average factor (used when ``alpha > 0``).
 
         Returns
         -------
-        UCBState
+        GradientBanditState
             Updated agent state.
         """
 
-        def classic_update(operands: Tuple) -> UCBState:
-            state, action, reward, alpha = operands
-            return UCBState(
-                Q=state.Q.at[action].add((reward - state.Q[action]) / state.N[action]),
-                N=state.N.at[action].add(1)
+        mask = jnp.ones_like(state.H, dtype=jnp.bool_).at[action].set(False)
+        pi = jax.nn.softmax(state.H)
+
+        R = jnp.where(state.n == 1, reward, state.R)
+
+        H = state.H
+        H = jnp.where(mask, H, H + lr * (reward - R) * (1 - pi))
+        H = jnp.where(mask, H - lr * (reward - R) * pi, H)
+
+        def classic_update(operands: Tuple) -> GradientBanditState:
+            state, H, R, reward, alpha = operands
+            return GradientBanditState(
+                H=H,
+                R=R + (reward - R) / state.n,
+                n=state.n + 1
             )
 
-        def erwa_update(operands: Tuple) -> UCBState:
-            state, action, reward, alpha = operands
-            return UCBState(
-                Q=state.Q.at[action].add(alpha * (reward - state.Q[action])),
-                N=state.N.at[action].add(1)
+        def erwa_update(operands: Tuple) -> GradientBanditState:
+            state, H, R, reward, alpha = operands
+            return GradientBanditState(
+                H=H,
+                R=R + alpha * (reward - R),
+                n=state.n + 1
             )
 
-        return jax.lax.cond(alpha == 0, classic_update, erwa_update, (state, action, reward, alpha))
+        return jax.lax.cond(alpha == 0, classic_update, erwa_update, (state, H, R, reward, alpha))
 
     @staticmethod
     def sample(
-        state: UCBState,
-        key: PRNGKey,
-        time: Scalar,
-        c: Scalar
-    ) -> Tuple[UCBState, jnp.int32]:
+        state: GradientBanditState,
+        key: PRNGKey
+    ) -> Tuple[GradientBanditState, jnp.int32]:
         """
         Selects next action based on current agent state.
 
         Parameters
         ----------
-        state : UCBState
+        state : GradientBanditState
             Current state of the agent.
         key : PRNGKey
             A PRNG key used as the random key.
-        time : float
-            Current time.
-        c : float
-            Degree of exploration.
 
         Returns
         -------
-        tuple[UCBState, jnp.int32]
+        tuple[GradientBanditState, jnp.int32]
             Tuple containing updated agent state and selected action.
         """
 
-        return state, jnp.argmax(state.Q + c * jnp.sqrt(jnp.log(time) / state.N))
+        return state, distrax.Categorical(state.H).sample(seed=key)
