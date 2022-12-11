Fix Softmax update rule

reinforced_lib/agents/softmax.py
@@ -64,7 +64,7 @@ class Softmax(BaseAgent):
         self.n_arms = n_arms
 
         self.init = jax.jit(partial(self.init, n_arms=n_arms))
-        self.update = jax.jit(partial(self.update, lr=lr, alpha=alpha))
+        self.update = jax.jit(partial(self.update, lr=lr, alpha=alpha, tau=tau))
         self.sample = jax.jit(partial(self.sample, tau=tau))
 
     @staticmethod
@@ -125,7 +125,8 @@ class Softmax(BaseAgent):
         action: jnp.int32,
         reward: Scalar,
         lr: Scalar,
-        alpha: Scalar
+        alpha: Scalar,
+        tau: Scalar
     ) -> SoftmaxState:
         """
         Updates the state of the agent after performing some action and receiving a reward.
@@ -144,6 +145,8 @@ class Softmax(BaseAgent):
             Step size.
         alpha : float
             Exponential recency-weighted average factor (used when ``alpha > 0``).
+        tau : float
+            Temperature parameter.
 
         Returns
         -------
@@ -153,10 +156,10 @@ class Softmax(BaseAgent):
 
         r = jnp.where(state.n == 1, reward, state.r)
         mask = jnp.ones_like(state.H, dtype=jnp.bool_).at[action].set(False)
-        pi = jax.nn.softmax(state.H)
+        pi = jax.nn.softmax(state.H / tau)
 
         return SoftmaxState(
-            H=state.H + lr * (reward - r) * jnp.where(mask, -pi, 1 - pi),
+            H=state.H + lr * (reward - r) * jnp.where(mask, pi, 1 - pi),
             r=r + (reward - r) * jnp.where(alpha == 0, 1 / state.n, alpha),
             n=state.n + 1
         )