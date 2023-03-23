d41beef7479b35745f2da8306c335d0211616ea1
Krzysztof Rusek <krusek@agh.edu.pl>
2023-03-23 19:35:12 +0100

    Update tf-light api

--- examples/lite/lite.py
+++ examples/lite/lite.py
@@ -1,3 +1,5 @@
+import sys
+
 import tensorflow as tf
 #tf.config.set_visible_devices([], 'GPU')
 
@@ -6,42 +8,38 @@ import jax.numpy as jnp
 import numpy as np
 import inspect
 from reinforced_lib.agents.mab import ThompsonSampling
+from functools import wraps
+import inspect
+import chex
+import jax.numpy as jnp
+import jax.tree_util as tree
 
+def flatten_args(tree_args_fun, treedef):
+    @wraps(tree_args_fun)
+    def flat_args_fun(*leaves):
+        tree_args = tree.tree_unflatten(treedef, leaves)
+        tree_ret = tree_args_fun(*tree_args)
+        return tree.tree_leaves(tree_ret)
 
-if __name__ == '__main__':
+    return flat_args_fun
 
+def make_converter(f,example):
+    """
 
+    Parameters
+    ----------
+    f a function
+    example arguments in tree format
+    """
+    leaves,treedef = tree.tree_flatten(example)
 
+    flat_fun = flatten_args(f,treedef)
 
-    # converter = tf.lite.TFLiteConverter.experimental_from_jax([lambda x: x+1],
-    #                                                           [[('x',jnp.ones(()))]])
-    # converter.target_spec.supported_ops = [
-    #     tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
-    #     tf.lite.OpsSet.SELECT_TF_OPS  # enable TensorFlow ops.
-    # ]
-    #
-    # tflite_update = converter.convert()
-    # with open('update.tflite', 'wb') as f:
-    #     f.write(tflite_update)
-    #
-    # interpreter = tf.lite.Interpreter(model_content=tflite_update)
-    # interpreter.allocate_tensors()
-    # input_details = interpreter.get_input_details()
-    # output_details = interpreter.get_output_details()
-    #
-    # args = jax.tree_util.tree_map(jnp.asarray,[2])
-    #
-    # for a,d in zip(args,input_details):
-    #     interpreter.set_tensor(d['index'],a)
-    #
-    # interpreter.invoke()
+    inputs = [[ (f'arg{i}',l) for i,l in enumerate(leaves)]]
+    converter = tf.lite.TFLiteConverter.experimental_from_jax([flat_fun],inputs)
+    return converter
 
-
-
-    # rl = RLib(
-    #     agent_type=ThompsonSampling,
-    #     ext_type=IEEE_802_11_ax_RA
-    # )
+def main():
     k = jax.random.PRNGKey(42)
     ts = ThompsonSampling(16)
 
@@ -49,33 +47,8 @@ if __name__ == '__main__':
     new_state = ts.update(state,k,1,2,1,0.1)
     a = ts.sample(new_state,k,1.)
 
-    k = jax.random.PRNGKey(42)
-    converter = tf.lite.TFLiteConverter.experimental_from_jax([ts.init], [[('k', k)]])
-    converter.target_spec.supported_ops = [
-        tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
-        #tf.lite.OpsSet.SELECT_TF_OPS  # enable TensorFlow ops.
-    ]
+    converter = make_converter(ts.update,tree.tree_map(jnp.asarray,[state,k,1,2,1,0.1]))
 
-    tflite_init = converter.convert()
-
-    arrays, defs = jax.tree_util.tree_flatten(state)
-
-    #inspect.signature(ts.update)
-    sig = inspect.signature(ts.update)
-
-    def _update(*args):
-        s = jax.tree_util.tree_unflatten(defs, args[:2])
-        arrs,_ = jax.tree_util.tree_flatten(ts.update(s,*args[2:]))
-
-
-    converter = tf.lite.TFLiteConverter.experimental_from_jax([_update],
-                                                              [[('alpha', state.alpha),
-                                                                ('beta', state.beta),
-                                                                ('k', k),
-                                                                ('action', jnp.asarray(1)),
-                                                                ('n_successful',jnp.asarray(1)),
-                                                                ("n_failed",jnp.asarray(0)),
-                                                                ("delta_time",jnp.asarray(0.1))]])
     converter.target_spec.supported_ops = [
         tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
         tf.lite.OpsSet.SELECT_TF_OPS  # enable TensorFlow ops.
@@ -90,24 +63,19 @@ if __name__ == '__main__':
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
-    # working END ____________
 
-    args = jax.tree_util.tree_leaves(state)+jax.tree_util.tree_map(jnp.asarray,[k,1,2,1,0.1])
-
-    for a,d in zip(args,input_details):
-        interpreter.set_tensor(d['index'],a)
+    # args = tree.tree_leaves([state,k,1,2,1,0.1])
+    # args = tree.tree_map(jnp.asarray,args)
+    #
+    # for a,d in zip(args,input_details):
+    #     interpreter.set_tensor(d['index'],a)
 
     interpreter.invoke()
 
+    return
 
-    converter = tf.lite.TFLiteConverter.experimental_from_jax([ts.sample],
-                                                              [[('state', state), ('k', k), ('context', 1)]])
+if __name__ == '__main__':
 
-    converter.target_spec.supported_ops = [
-        tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
-        tf.lite.OpsSet.SELECT_TF_OPS  # enable TensorFlow ops.
-    ]
-
-    tflite_update = converter.convert()
+    main()
 
     ...
