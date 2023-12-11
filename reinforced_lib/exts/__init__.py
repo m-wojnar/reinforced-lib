d0c389c0f8942720c187644641f8887b841569f4
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2023-12-12 00:00:24 +0100

    Make it easier to import the BasicMab extension

--- reinforced_lib/exts/__init__.py
+++ reinforced_lib/exts/__init__.py
@@ -1,3 +1,4 @@
 from reinforced_lib.exts.utils import observation, parameter
 from reinforced_lib.exts.base_ext import BaseExt
+from reinforced_lib.exts.basic_mab import BasicMab
 from reinforced_lib.exts.gymnasium import Gymnasium
