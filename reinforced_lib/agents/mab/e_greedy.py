ca85a29878a25c3957ea859bea677ec89c281384
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-10-17 14:16:55 +0200

    Add epsilon decay in e-greedy MAB

--- reinforced_lib/agents/mab/e_greedy.py
+++ reinforced_lib/agents/mab/e_greedy.py
@@ -19,10 +19,13 @@ class EGreedyState(AgentState):
         Action-value function estimates for each arm.
     N : Array
         Number of tries for each arm.
+    e : float
+        Experiment rate (epsilon).
     """
 
     Q: Array
     N: Array
+    e: Scalar
 
 
 class EGreedy(BaseAgent):
@@ -30,13 +33,18 @@ class EGreedy(BaseAgent):
     Epsilon-greedy [5]_ agent with an optimistic start behavior and optional exponential recency-weighted average update.
     It selects a random action from a set of all actions :math:`\mathscr{A}` with probability
     :math:`\epsilon` (exploration), otherwise it chooses the currently best action (exploitation).
+    Epsilon can be decayed over time to shift the policy from exploration to exploitation.
 
     Parameters
     ----------
     n_arms : int
         Number of bandit arms. :math:`N \in \mathbb{N}_{+}`.
     e : float
-        Experiment rate (epsilon). :math:`\epsilon \in [0, 1]`.
+        Initial experiment rate (epsilon). :math:`\epsilon \in [0, 1]`.
+    e_min : float, default=0.0
+        Minimum value of the experiment rate. :math:`\epsilon_{\min} \in [0, 1]`.
+    e_decay : float, default=1.0
+        Decay factor for the experiment rate. :math:`\epsilon_{\text{decay}} \in [0, 1]`.
     optimistic_start : float, default=0.0
         Interpreted as the optimistic start to encourage exploration in the early stages.
     alpha : float, default=0.0
@@ -51,23 +59,29 @@ class EGreedy(BaseAgent):
             self,
             n_arms: int,
             e: Scalar,
+            e_min: Scalar = 0.0,
+            e_decay: Scalar = 1.0,
             optimistic_start: Scalar = 0.0,
             alpha: Scalar = 0.0
     ) -> None:
         assert 0 <= e <= 1
+        assert 0 <= e_min <= 1
+        assert 0 <= e_decay <= 1
         assert 0 <= alpha <= 1
 
         self.n_arms = n_arms
 
-        self.init = jax.jit(partial(self.init, n_arms=n_arms, optimistic_start=optimistic_start))
-        self.update = jax.jit(partial(self.update, alpha=alpha))
-        self.sample = jax.jit(partial(self.sample, e=e))
+        self.init = jax.jit(partial(self.init, n_arms=n_arms, e=e, optimistic_start=optimistic_start))
+        self.update = jax.jit(partial(self.update, alpha=alpha, e_decay=e_decay, e_min=e_min))
+        self.sample = jax.jit(self.sample)
 
     @staticmethod
     def parameter_space() -> gym.spaces.Dict:
         return gym.spaces.Dict({
             'n_arms': gym.spaces.Box(1, jnp.inf, (1,), int),
             'e': gym.spaces.Box(0.0, 1.0, (1,), float),
+            'e_min': gym.spaces.Box(0.0, 1.0, (1,), float),
+            'e_decay': gym.spaces.Box(0.0, 1.0, (1,), float),
             'optimistic_start': gym.spaces.Box(0.0, jnp.inf, (1,), float),
             'alpha': gym.spaces.Box(0.0, 1.0, (1,), float)
         })
@@ -91,6 +105,7 @@ class EGreedy(BaseAgent):
     def init(
             key: PRNGKey,
             n_arms: int,
+            e: Scalar,
             optimistic_start: Scalar
     ) -> EGreedyState:
         r"""
@@ -103,6 +118,8 @@ class EGreedy(BaseAgent):
             A PRNG key used as the random key.
         n_arms : int
             Number of bandit arms.
+        e : float
+            Initial experiment rate (epsilon).
         optimistic_start : float
             Interpreted as the optimistic start to encourage exploration in the early stages.
 
@@ -114,7 +131,8 @@ class EGreedy(BaseAgent):
 
         return EGreedyState(
             Q=jnp.full((n_arms, 1), optimistic_start),
-            N=jnp.ones((n_arms, 1), dtype=int)
+            N=jnp.ones((n_arms, 1), dtype=int),
+            e=e
         )
 
     @staticmethod
@@ -123,13 +141,17 @@ class EGreedy(BaseAgent):
         key: PRNGKey,
         action: int,
         reward: Scalar,
-        alpha: Scalar
+        alpha: Scalar,
+        e_decay: Scalar,
+        e_min: Scalar
     ) -> EGreedyState:
         r"""
         In the stationary case, the action-value estimate for a given arm is updated as
         :math:`Q_{t + 1} = Q_t + \frac{1}{t} \lbrack R_t - Q_t \rbrack` after receiving reward :math:`R_t` at step
         :math:`t` and the number of tries for the corresponding arm is incremented. In the non-stationary case,
         the update follows the equation :math:`Q_{t + 1} = Q_t + \alpha \lbrack R_t - Q_t \rbrack`.
+        Exploration rate :math:`\epsilon` can be decayed over time following the equation
+        :math:`\epsilon_{t + 1} = \max(\epsilon_{\min}, \epsilon_t \times \epsilon_{\text{decay}})`.
 
         Parameters
         ----------
@@ -143,6 +165,10 @@ class EGreedy(BaseAgent):
             Reward collected by the agent after taking the previous action.
         alpha : float
             Exponential recency-weighted average factor (used when :math:`\alpha > 0`).
+        e_decay : float
+            Decay factor for the experiment rate.
+        e_min : float
+            Minimum value of the experiment
 
         Returns
         -------
@@ -154,14 +180,16 @@ class EGreedy(BaseAgent):
             state, action, reward, alpha = operands
             return EGreedyState(
                 Q=state.Q.at[action].add((reward - state.Q[action]) / state.N[action]),
-                N=state.N.at[action].add(1)
+                N=state.N.at[action].add(1),
+                e=jnp.maximum(e_min, state.e * e_decay)
             )
 
         def erwa_update(operands: tuple) -> EGreedyState:
             state, action, reward, alpha = operands
             return EGreedyState(
                 Q=state.Q.at[action].add(alpha * (reward - state.Q[action])),
-                N=state.N.at[action].add(1)
+                N=state.N.at[action].add(1),
+                e=jnp.maximum(e_min, state.e * e_decay)
             )
 
         return jax.lax.cond(alpha == 0, classic_update, erwa_update, (state, action, reward, alpha))
@@ -169,8 +197,7 @@ class EGreedy(BaseAgent):
     @staticmethod
     def sample(
         state: EGreedyState,
-        key: PRNGKey,
-        e: Scalar
+        key: PRNGKey
     ) -> int:
         r"""
         Epsilon-greedy agent follows the policy:
@@ -188,8 +215,6 @@ class EGreedy(BaseAgent):
             Current state of the agent.
         key : PRNGKey
             A PRNG key used as the random key.
-        e : float
-            Experiment rate (epsilon).
 
         Returns
         -------
@@ -198,5 +223,5 @@ class EGreedy(BaseAgent):
         """
 
         max_q = (state.Q == state.Q.max()).astype(float)
-        probs = (1 - e) * max_q / jnp.sum(max_q) + e / state.Q.shape[0]
+        probs = (1 - state.e) * max_q / jnp.sum(max_q) + state.e / state.Q.shape[0]
         return jax.random.choice(key, state.Q.shape[0], p=probs.flatten())
